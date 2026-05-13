142ddb33d4a23ef204fc21b2f337dfa599ee062a
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-05-13 17:09:15 +0200

    Initial implementation of CalculateResultantTemperature (WIP)

---
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Modify/CalculateResultantTemperature.cs
@@ -0,0 +1,13 @@
+﻿using TPD;
+
+namespace SAM.Analytical.Tas.TPD
+{
+    public static partial class Modify
+    {
+        public static bool CalculateResultantTemperature(string path_TPD)
+        {
+            return true;
+        }
+
+    }
+}
